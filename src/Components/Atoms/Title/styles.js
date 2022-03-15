9802209779c076b1d21625b85cc1d039aee66d97
Victor Luiz <c0007043@vli-logistica.com.br>
2022-03-14 21:29:09 -0300

    Criado o style

---
+++ src/Components/Atoms/Title/styles.js
@@ -0,0 +1,15 @@
+import styled from 'styled-components'
+
+export const TitleSpan = styled.span`
+  align-items: center;
+  justify-content: center;
+  font-size: 60px;
+  text-align: center;
+  font-family: Fredoka;
+`;
+
+export const TitleContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
