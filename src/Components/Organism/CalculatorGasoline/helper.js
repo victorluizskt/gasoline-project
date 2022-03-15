9cb2d3d911b993be49ee4219eed127b3b4f5acfc
Victor Luiz <c0007043@vli-logistica.com.br>
2022-03-14 22:22:36 -0300

    Resolvendo erros

--- src/Components/Organism/CalculatorGasoline/helper.js
+++ src/Components/Organism/CalculatorGasoline/helper.js
@@ -1,8 +1,8 @@
 export const calculateTotal = (kilometerLiter, priceGasoline, totalRoad) => {
-  if(kilometerLiter, priceGasoline, totalRoad) {
+  if(kilometerLiter && priceGasoline && totalRoad) {
     const totalLiter = totalRoad / kilometerLiter;
     return totalLiter * priceGasoline;
-  }
+  };
 
   return "Values invalid";
 }
